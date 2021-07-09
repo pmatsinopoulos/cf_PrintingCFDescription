2b6a04b049e6ca13b72039af6b67f87b85ccfc40
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-07-09 08:15:39 +0300

    Initial Commit

--- PrintDescription/main.c
+++ PrintDescription/main.c
@@ -6,9 +6,31 @@
 //
 
 #include <stdio.h>
+#include <CoreFoundation/CoreFoundation.h>
+
+void describe255(CFTypeRef cfObject) {
+  char buffer[256];
+  CFIndex got;
+  CFStringRef description = CFCopyDescription(cfObject);
+  CFStringGetBytes(description,
+                   CFRangeMake(0, CFStringGetLength(description)),
+                   CFStringGetSystemEncoding(),
+                   '?',
+                   TRUE,
+                   (UInt8 *)buffer,
+                   255,
+                   &got);
+  buffer[got] = (char)0;
+  printf("%s", buffer);
+  CFRelease(description);
+}
 
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  printf("Hello, World!\n");
+  CFStringRef myObject = CFSTR("Hello World");
+  
+  describe255(myObject);
+  
+  CFRelease(myObject);
+  
   return 0;
 }
